f767b3993aff67523dbc89bc912e89ed97a2a074
LoperaUr <manuellopera612@gmail.com>
2023-09-22 17:16:54 -0500

    change name of UserRepository

--- src/main/java/com/pgm/verboseoctobroccoli/infrastructure/output/jpa/repository/UserRepositoryImpl.java
+++ src/main/java/com/pgm/verboseoctobroccoli/infrastructure/output/jpa/repository/UserRepositoryImpl.java
@@ -1,12 +1,12 @@
 package com.pgm.verboseoctobroccoli.infrastructure.output.jpa.repository;
 
-import com.pgm.verboseoctobroccoli.domain.spi.IoCTestUserRepo;
+import com.pgm.verboseoctobroccoli.domain.spi.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 @RequiredArgsConstructor
 @Repository
-public class UserRepositoryImpl implements IoCTestUserRepo {
+public class UserRepositoryImpl implements UserRepository {
     private final IUserRepository userRepository;
 
     @Override
