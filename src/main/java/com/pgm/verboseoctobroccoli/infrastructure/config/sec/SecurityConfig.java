87b3f3c7aa3b653c54beae26e09c13c0e69af14a
LoperaUr <manuellopera612@gmail.com>
2023-09-22 17:17:54 -0500

    refactor roles to constants

--- src/main/java/com/pgm/verboseoctobroccoli/infrastructure/config/sec/SecurityConfig.java
+++ src/main/java/com/pgm/verboseoctobroccoli/infrastructure/config/sec/SecurityConfig.java
@@ -21,6 +21,9 @@ public class SecurityConfig {
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
     private final AuthenticationProvider authProvider;
 
+    public static final String ROLE_ADMIN = "ADMIN";
+    public static final String ROLE_CLIENT = "CLIENT";
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
@@ -36,8 +39,10 @@ public class SecurityConfig {
                                         "v3/api-docs/**"
                                 ).permitAll()
                                 .requestMatchers("auth/**").permitAll()
-                                .requestMatchers(POST, "categories/").hasAnyAuthority("ADMIN")
-                                .requestMatchers(GET, "categories/").hasAnyAuthority("CLIENT", "ADMIN")
+                                .requestMatchers(POST, "categories/").hasAnyAuthority(ROLE_ADMIN)
+                                .requestMatchers(GET, "categories/").hasAnyAuthority(ROLE_CLIENT, ROLE_ADMIN)
+                                .requestMatchers(POST, "users/").hasAnyAuthority(ROLE_ADMIN)
+                                .requestMatchers(GET, "users/").hasAuthority(ROLE_ADMIN)
                                 .anyRequest().authenticated()
                 )
                 .sessionManagement(sessionManager ->
