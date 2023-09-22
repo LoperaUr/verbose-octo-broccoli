1fdd75867510416d0f17fedefb6e7fe9f6aaf1ab
LoperaUr <manuellopera612@gmail.com>
2023-09-22 17:16:18 -0500

    CategoryRepository implement

--- src/main/java/com/pgm/verboseoctobroccoli/infrastructure/output/jpa/repository/ICategoryRepository.java
+++ src/main/java/com/pgm/verboseoctobroccoli/infrastructure/output/jpa/repository/ICategoryRepository.java
@@ -3,4 +3,6 @@ package com.pgm.verboseoctobroccoli.infrastructure.output.jpa.repository;
 import com.pgm.verboseoctobroccoli.infrastructure.output.jpa.entity.CategoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ICategoryRepository extends JpaRepository<CategoryEntity, Long> { }
+public interface ICategoryRepository extends JpaRepository<CategoryEntity, Long> {
+    boolean existsByName(String name);
+}
