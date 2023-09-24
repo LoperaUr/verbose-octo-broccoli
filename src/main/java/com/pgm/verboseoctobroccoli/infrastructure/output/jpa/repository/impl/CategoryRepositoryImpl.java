1296aab96f8528e73f422607c6403d6bb7af6957
LoperaUr <manuellopera612@gmail.com>
2023-09-23 19:44:47 -0500

    impl repo move

--- src/main/java/com/pgm/verboseoctobroccoli/infrastructure/output/jpa/repository/impl/CategoryRepositoryImpl.java
+++ src/main/java/com/pgm/verboseoctobroccoli/infrastructure/output/jpa/repository/impl/CategoryRepositoryImpl.java
@@ -1,6 +1,7 @@
-package com.pgm.verboseoctobroccoli.infrastructure.output.jpa.repository;
+package com.pgm.verboseoctobroccoli.infrastructure.output.jpa.repository.impl;
 
 import com.pgm.verboseoctobroccoli.domain.spi.repo.CategoryRepository;
+import com.pgm.verboseoctobroccoli.infrastructure.output.jpa.repository.ICategoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
